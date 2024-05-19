cdc2e9512d9dc2810a1cb1844fb3b9dfa21ad429
Pedro <coelhopedro152@gmail.com>
2024-05-19 19:16:20 -0300

    subindo-projeto

---
+++ API-REST_GO-main/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/pedro-coelho1604/go-rest-api/database"
+	"github.com/pedro-coelho1604/go-rest-api/models"
+	"github.com/pedro-coelho1604/go-rest-api/routes"
+)
+
+func main() {
+	models.Personalidades = []models.Personalidade{
+		{Id: 1, Nome: "Nome 1", Historia: "Historia 1"},
+		{Id: 2, Nome: "Nome 2", Historia: "Historia 2"},
+	}
+
+	database.ConectaComBancoDeDados()
+	fmt.Println("Iniciando o servidor Rest com Go")
+	routes.HandleResquest()
+}
